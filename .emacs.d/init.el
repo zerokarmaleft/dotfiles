3c6f89bc1bc9e8e9db07522a398d9b6d2d313c78
Edward Cho <zerokarmaleft@gmail.com>
2014-06-08 08:11:24 -0500

    enable pretty-mode for JavaScript

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -190,6 +190,7 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 (customize-set-variable 'js2-basic-offset 2)
+(add-hook 'js2-mode-hook 'pretty-mode)
 
 ;; ===========================================================================
 ;; Common Lisp / SLIME
